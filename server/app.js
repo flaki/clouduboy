61945682f33dd6c5bc8090d1d6b46e4594f318e2
Flaki <git@flaki.hu>
2015-12-28 18:38:24 +0100

    Preparations for build-support

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
+const DIR_ROOT = require('path').normalize(__dirname+"/..");
+const DIR_SOURCES = DIR_ROOT + "/sources";
+
 app.get('/', function (req, res) {
-  res.send('Clouduboy Cloud Compiler v0.1.1');
+  res.send('Clouduboy Cloud Compiler v0.1.2');
 });
 
 app.get('/build', function (req, res) {
-  res.send(fs.readFileSync('../arduino-rund-ino/rund/rund.ino.leonardo.hex'));
+  res.send(fs.readFileSync(DIR_SOURCES + '/arduboy-rund-ino/rund/rund.ino.leonardo.hex'));
 });
 
 var server = app.listen(3204, function () {

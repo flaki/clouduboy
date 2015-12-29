cc779a9dec55e9069dd8d32c4113aac9ebff9261
Flaki <git@flaki.hu>
2015-12-29 03:28:44 +0100

    Add build POST API

--- server/app.js
+++ server/app.js
@@ -6,11 +6,20 @@ const DIR_ROOT = require('path').normalize(__dirname+"/..");
 const DIR_SOURCES = DIR_ROOT + "/sources";
 
 app.get('/', function (req, res) {
-  res.send('Clouduboy Cloud Compiler v0.1.2');
+  res.send('Clouduboy Cloud Compiler v0.1.3');
 });
 
 app.get('/build', function (req, res) {
   res.send(fs.readFileSync(DIR_SOURCES + '/arduboy-rund-ino/rund/rund.ino.leonardo.hex'));
+});
+
+app.get('/post', function (req, res) {
+  var hex = fs.readFileSync(DIR_SOURCES + '/arduboy-rund-ino/rund/rund.ino.leonardo.hex');
+  res.json({
+    "result": {
+      hex: hex
+    }
+  });
 });
 
 var server = app.listen(3204, function () {

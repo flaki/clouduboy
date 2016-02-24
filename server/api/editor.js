2360e51356a211b3de251441fd659eaf274da347
Flaki <git@flaki.hu>
2016-02-24 16:55:07 +0100

    Initial support for "dist" builds

--- server/api/editor.js
+++ server/api/editor.js
@@ -10,5 +10,12 @@ const CFG = require('../cfg.js');
 
 
 function all(req, res) {
-  res.sendFile(CFG.WEB_DIR + '/editor.html');
+  let editor = CFG.WEB_DIR + '/editor.html';
+
+  // If [dist] version requested modify the editor.html to point to [dist] sources
+  if (req.query.dist) {
+    return res.send(require('fs').readFileSync(editor).toString().replace(/\/js\//g,'/dist/js/'));
+  }
+
+  res.sendFile(editor);
 }

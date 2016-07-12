4603bea1071e8ebaee8b7ea5c418e04be7710d82
Flaki <git@flaki.hu>
2016-07-12 17:51:54 +0200

    Updated "Sprites" template for X-platform support

---
+++ templates/sprites/pif.js
@@ -0,0 +1,325 @@
+/* PixelData manipulation and storage library
+*/
+(function() {
+
+function PixelData(input) {
+  // Buffer object - should be some binary image file.
+  // Supported import formats: BMP
+  if (typeof input === 'object' && typeof Buffer !== 'undefined' && input instanceof Buffer) {
+    try {
+      this.bitmap = bmp2bitmap(input);
+      this.w = this.bitmap[0].length;
+      this.h = this.bitmap.length;
+      return;
+    } catch(e) {
+
+    }
+
+    // Not any of the supported binary formats, convert to string
+    input = input.toString();
+  }
+
+  // Could be serialized JSON
+  if (typeof input === 'string') {
+    try {
+      input = JSON.parse(input);
+    } catch (e) {
+      // don't fret, could still be a PIF
+    }
+  }
+
+  // Object, should have w/h set and some data field
+  // If object data is not byte data continue with that data as input
+  if (typeof input === 'object' && input.data instanceof Array) {
+    if (typeof input.data[0] === 'number') {
+      this.bitmap = bytes2bitmap(input.data, input.w, input.h, input.frames, (input.w && input.h && "explicit-size") );
+      this.frames = input.frames;
+      this.w = this.bitmap[0].length / (input.frames||1);
+      this.h = this.bitmap.length;
+      this.id = input.id;
+      return;
+    } else {
+      input = input.data;
+    }
+  }
+
+  // String, try parsing as PIF
+  if (typeof input === 'string') {
+    try {
+      this.bitmap = pif2bitmap(input);
+      this.w = this.bitmap[0].length;
+      this.h = this.bitmap.length;
+
+      // Try to read embedded id tag
+      try {
+        this.id = input.match(/\:(\w+)/)[1];
+      } catch (e) {
+        console.log('No embedded id found in PIF string');
+      }
+      return;
+
+    } catch (e) {
+      console.log('Invalid PIF data!');
+      throw e;
+    }
+  }
+
+  // Nested array - should be a bitmap already
+  if (input instanceof Array && input[0] instanceof Array) {
+    this.bitmap = input;
+    this.w = this.bitmap[0].length;
+    this.h = this.bitmap.length;
+    return;
+  }
+
+  throw("Unrecognized file format.");
+}
+
+PixelData.prototype = {
+  get pif() {
+    // frames support
+    if ('$f' in this) {
+      return bitmap2pif(
+        this.bitmap
+          .map(row => row.slice(this.$f*this.w, (this.$f+1)*this.w))
+        , (this.id ? this.id+'_'+this.$f : void 0)
+      );
+    }
+    return bitmap2pif(this.bitmap, this.id);
+  },
+  get bytes() {
+    return bitmap2bytes(this.bitmap);
+  },
+  get rgba() {
+    return bitmap2rgba(this.bitmap)
+  },
+  get sprite() {
+    return this.bytes.reduce(function(sprite, v) {
+      return sprite += (sprite === '' ? '' : ', ') + ( v<16 ? '0x0' : '0x' ) + v.toString(16);
+    }, '');
+  },
+
+  // Compare two PixelData objects
+  equals: function(other) {
+    try {
+      return this.pif === other.pif;
+    } catch (e) {
+      console.log("Error matching PixelData: ", e);
+      return false;
+    }
+  },
+
+  // A "view" into the PixelData object, representing just a single frame
+  frame: function(n) {
+    if (n > (this.frames||0)) return this;
+
+    var fobj = Object.create(this);
+    fobj.$f = n;
+    return fobj;
+  },
+
+  // Create an object with all properties
+  serialize: function() {
+    return {
+      id: this.id,
+      w: this.w,
+      h: this.h,
+      data: this.bytes
+    };
+  }
+}
+
+
+function bitmap2pif(bitmap, id) {
+  return (id ? ':' + id + ':\n' : '') +
+    bitmap.reduce(function(out,row) {
+      out.push(row.join(''));
+      return out;
+    },[]).join('\n').replace(/0/g,'.').replace(/1/g,'#');
+}
+
+function pif2bitmap(pif) {
+  var rows = pif.replace(/[^\.\#]+/g,' ').trim().split(/\s+/g);
+
+  var matrix = rows.map(function(s) {
+    var i,
+        r = [];
+
+    for (i = 0; i < s.length; ++i) {
+      r.push(s[i] === '#' ? 1 : 0);
+    }
+
+    return r;
+  });
+
+  return matrix;
+}
+
+function pif2bytes(pif) {
+  return bitmap2bytes( pif2bitmap( pif ));
+}
+
+function bitmap2bytes(bitmap) {
+  var x,y,seg,ymax,v;
+  var w = bitmap[0].length, h = bitmap.length;
+  var bytes = [];
+
+  seg = 0;
+  while (seg < h) {
+    for (x = 0; x < w; ++x) {
+      v = 0;
+      ymax = (h-seg < 8 ? h-seg : 8);
+
+      for (y = 0; y < ymax; ++y) {
+        v |= bitmap[seg+y][x] << y;
+      }
+
+      // Save byte values
+      bytes.push(v);
+    }
+
+    seg += 8;
+  }
+
+  return bytes;
+}
+
+function bitmap2rgba(bitmap, fg, bg) {
+  var w = bitmap[0].length, h = bitmap.length;
+  var rgba = new Uint8ClampedArray(w * h * 4);
+  var bg = bg || [0,0,0,0];
+  var fg = fg || [255,255,255,255];
+  var cc;
+
+
+  var y = 0;
+  while (y < h) {
+
+    for (x = 0; x < w; ++x) {
+      cc = (bitmap[y][x] ? fg : bg);
+
+      rgba[ (y*w + x)*4 + 0 ] = cc[0];
+      rgba[ (y*w + x)*4 + 1 ] = cc[1];
+      rgba[ (y*w + x)*4 + 2 ] = cc[2];
+      rgba[ (y*w + x)*4 + 3 ] = cc[3];
+    }
+
+    ++y;
+  }
+
+  return rgba;
+}
+
+
+function bytes2bitmap(bytes,w,h,frames,explicit) {
+  // Width / height optional, assume square image @ 8x8 or multiples
+  if (!explicit) {
+    if (!h) {
+      h = w;
+    }
+    if (!w || (w * Math.ceil(h/8)) < bytes.length) {
+      w = h = Math.ceil( Math.sqrt( bytes.length / 8) ) * 8;
+    }
+  }
+
+  var x,y,f;
+  var bitmap = [];
+
+  f = 0;
+  do {
+
+    for (y = 0; y<h; ++y) {
+      bitmap[y] = bitmap[y] || [];
+      for (x = 0; x<w; ++x) {
+        bitmap[y][x + f*w] = (bytes[w * (f + (y >> 3)) + x] & (1 << (y % 8))) ? 1 : 0;
+      }
+    }
+
+    ++f;
+  } while (f < frames);
+
+  return bitmap;
+}
+
+function bytes2pif(bytes,w,h,id) {
+  return bitmap2pif( bytes2bitmap(bytes,w,h), id );
+}
+
+// Import BMP bitmap files
+// Based on the BMP data format structure documented in:
+// http://paulbourke.net/dataformats/bmp/
+function bmp2bitmap(b) {
+  // BM (magic identifier) in fist two bytes: 0x42@0, 0x4D@1
+  if (b[0] !== 0x42 || b[1] !== 0x4D) throw("Not a valid BMP file.");
+
+  // Image data starts: 4-byte-uint@10
+  var imgdata = b.readUInt32LE(10);
+
+  // Image width & height: 4-byte-int@18,22
+  var w = b.readInt32LE(18),
+      h = b.readInt32LE(22);
+
+  // Bits-per-pixel: 2-byte-ushort@28
+  // 32bpp means alpha channel + RGB
+  var bpp = b.readUInt16LE(28);
+
+  // Log import
+  console.log("Importing BMP bitmap:", b.length,"bytes,",w,"x",h,"bitmap @",bpp,"bpp");
+
+  // Read image bitmap based on above metadata
+  var bitmap = new Array(h),
+      y = 0, v;
+
+  for (i = 0; i < w*h; ++i) {
+    // For some reason the image rows are flipped in BMP files
+    y = h - 1 - Math.floor(i/w);
+
+    // New row
+    if (!bitmap[y]) bitmap[y] = [];
+
+    switch (bpp) {
+      // 32 bit ABGR
+      case 32:
+        v = b.readUInt32LE(imgdata + i * 4);
+
+        // Check transparency value, also non-black color value
+        bitmap[y][i%w] = ( (v >>> 24) > (0xFF >> 1) && (v & 0xFFFFFF) ) ? 1 : 0;
+        break;
+
+      // 24 bit BGR
+      case 24:
+        v = b[imgdata + i*3] | b[imgdata + i*3 + 1] | b[imgdata + i*3 + 2];
+
+        // Count all non-completely-black color values
+        bitmap[y][i%w] = v ? 1 : 0;
+        break;
+
+      // 1 bit B&W
+      case 1:
+        // Strange padding to 4 bytes
+        v = b.readUInt32BE(imgdata + (h - y - 1) * Math.ceil(w/32)*4);
+
+        // Count all bits
+        bitmap[y][i%w] = ( (v >>> (31-(i%w))) & 1 ) ? 1 : 0;
+        break;
+    }
+  }
+
+  return bitmap;
+}
+
+/*
+GLOBAL.fromBitmap = bitmap2pif;
+GLOBAL.toBitmap = pif2bitmap;
+
+GLOBAL.fromBytes = bytes2pif;
+GLOBAL.toBytes = pif2bytes;
+*/
+
+try {
+  window.PixelData = PixelData;
+} catch (e) {
+  module.exports = PixelData;
+}
+
+})(); // IIFE
--- templates/sprites/sprites.ino
+++ templates/sprites/sprites.ino
@@ -1,39 +1,40 @@
 #include "Arduboy.h"
 
 
-// Pre-drawn pixelart animation - Flappy Bat (17x7, 2 frames)
-PROGMEM const unsigned char sprite1[] = { /*17x7*/ 0x1c, 0x0e, 0x0c, 0x07, 0x0e, 0x0c, 0x1b, 0x3e, 0x3e, 0x1b, 0x0c, 0x06, 0x0f, 0x1c, 0x0e, 0x1c, 0x38 };
+// Pre-drawn pixelart animation - Flappy Bat (16x6, 2 frames)
+PROGMEM const unsigned char bats[] = { /*16x6x2*/
+  0x01, 0x0f, 0x0e, 0x1c, 0x38, 0x3c, 0x1b, 0x3e,
+  0x3e, 0x1b, 0x3c, 0x38, 0x1c, 0x0e, 0x0f, 0x01,
+  0x1c, 0x0e, 0x0c, 0x07, 0x0e, 0x0c, 0x1b, 0x3e,
+  0x3e, 0x1b, 0x0c, 0x0e, 0x07, 0x0c, 0x0e, 0x1c,
+};
 
-PROGMEM const unsigned char sprite2[] = { /*17x7*/ 0x01, 0x0f, 0x0e, 0x1c, 0x38, 0x3c, 0x1b, 0x3e, 0x3e, 0x1b, 0x3c, 0x38, 0x1c, 0x0e, 0x0f, 0x01, 0x00 };
 
-
+// Initialize Arduboy
 Arduboy arduboy;
 
-int animationSpeed = 18;
-
-int animFrame = 0;
-int cSprite = 0;
-
 void setup() {
-  arduboy.start();
-  arduboy.setTextSize(3);
-  arduboy.setCursor(0,0);
-  arduboy.print("Sprite\nDemo");
-  arduboy.display();
+  arduboy.begin();
 }
 
+
+// Local vars
 int x = 0, y = 24;
 int sx = 1, sy = 1;
 
+int animationSpeed = 8;
+int cSprite = 0;
 
+
+// Main loop
 void loop () {
-  // pause render until it's time for the next frame
+  // Ensure correct frame rate
   if (!(arduboy.nextFrame()))
     return;
 
+
   // Update flapping animation
-  if (--animFrame <= 0) {
-    animFrame = animationSpeed;
+  if (arduboy.everyXFrames(animationSpeed)) {
     cSprite = !cSprite;
   }
 
@@ -46,16 +47,20 @@ void loop () {
 
 
   // Clear display, redraw background text
-  arduboy.clearDisplay();
+  arduboy.clear();
+
+  // Background text
+  arduboy.setTextSize(3);
   arduboy.setCursor(0,0);
   arduboy.print("Sprite\nDemo");
 
   // Draw shadow
-  arduboy.drawBitmap(0 +x,2 +y, cSprite ? sprite1 : sprite2, 17,7, BLACK);
-  arduboy.drawBitmap(2 +x,2 +y, cSprite ? sprite1 : sprite2, 17,7, BLACK);
+  arduboy.drawBitmap(0 +x,2 +y, bats +cSprite*16, 16,6, BLACK);
+  arduboy.drawBitmap(2 +x,2 +y, bats +cSprite*16, 16,6, BLACK);
 
   // Draw Bat
-  arduboy.drawBitmap(1 +x,1 +y, cSprite ? sprite1 : sprite2, 17,7, WHITE);
+  arduboy.drawBitmap(1 +x,1 +y, bats +cSprite*16, 16,6, WHITE);
+
 
   // Update Screen
   arduboy.display();

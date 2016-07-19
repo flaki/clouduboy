af37e44e826962f65ee489c38601c08dfd186c24
Flaki <git@flaki.hu>
2016-07-19 22:29:54 +0200

    Preliminary loop() processing, target refactor

--- microcanvas/build.js
+++ microcanvas/build.js
@@ -1,9 +1,23 @@
-'use strict';
+ 'use strict';
 
 const fs = require('fs');
 const acorn = require('acorn');
 
 let srcFile = process.argv[2] || '../templates/sprites/sprites.js';
+let targetSystem = process.argv[3] || 'arduboy';
+
+// Button mapping for different targets
+const BUTTONS = {
+  'arduboy': {
+    left: 'LEFT_BUTTON',
+    right: 'RIGHT_BUTTON',
+    up: 'UP_BUTTON',
+    down: 'DOWN_BUTTON',
+    space: 'A_BUTTON',
+    enter: 'B_BUTTON',
+  }
+}
+
 let src = fs.readFileSync(srcFile);
 
 let ast = acorn.parse(src ,{ ecmaVersion: 6, sourceType: 'script' });
@@ -11,20 +25,34 @@ let ast = acorn.parse(src ,{ ecmaVersion: 6, sourceType: 'script' });
 fs.writeFileSync('ast.json', JSON.stringify(ast));
 
 
-let game = { alias: 'game' };
+// Init
+let game = {
+  alias: 'game',
+  target: targetSystem,
+  constants: [], globals: [], gfx: [], sfx: []
+};
 
 
-parseGlobals();
+// Parse
+parseGlobals(); // TODO: function declarations!
 
 parseInitializers();
 
 parseSetupBody();
 
+parseLoopBody();
+
+// Build
+game.ino = exportGame(targetSystem);
+
+// Save
+fs.writeFileSync('game.json', JSON.stringify(game));
+
+fs.writeFileSync('game.ino', game.ino||'');
+
+
 
 function createConstant(id, value, type) {
-  // Make sure the constants list exists
-  if (!game.constants) game.constants = [];
-
   // If no type specified, try to guess it
   // PS: constants shouldn't be affected by scope issues
   //if (!type) type = guessType(id, value, 'constant');
@@ -53,7 +81,6 @@ function parseGlobals() {
   .filter(o => o.type === 'VariableDeclaration')
   .forEach(function (n) {
     if (n.kind === 'const') {
-
       n.declarations.forEach(dec => {
         createConstant(getString(dec.id), getString(dec.init));
       });
@@ -61,8 +88,6 @@ function parseGlobals() {
     } else if (n.kind === 'let') {
       n.declarations.forEach(dec => {
         if (dec.id.name.match(/^gfx/)) {
-          game.gfx = game.gfx || [];
-
           game.gfx.push({
             id: dec.id.name,
             cid: toSnakeCase(dec.id.name)
@@ -70,8 +95,6 @@ function parseGlobals() {
           game.gfx[dec.id.name] = game.gfx[game.gfx.length-1];
 
         } else if (dec.id.name.match(/^sfx/)) {
-          game.sfx = game.sfx || [];
-
           game.sfx.push({
             id: dec.id.name,
             cid: toSnakeCase(dec.id.name)
@@ -85,8 +108,6 @@ function parseGlobals() {
           console.log("MicroCanvas uses the alias: game")
 
         } else {
-          game.globals = game.globals || [];
-
           game.globals.push({
             id: dec.id.name,
             cid: toSnakeCase(dec.id.name),
@@ -147,7 +168,7 @@ function parseSetupBody() {
 
     // Try translating the line
     } else if (true) {
-      let ln = translate('arduboy', exp);
+      let ln = translate(exp);
       game.setup.code.push(ln);
       console.log('>>> '+ln);
 
@@ -157,13 +178,23 @@ function parseSetupBody() {
   });
 }
 
-game.ino = exportGame('arduboy');
+// Parse game.setup call body and generate setup
+function parseLoopBody() {
+  console.log('Processing game.loop()');
+  game.loop = { code: [] };
 
+  let loopbody = game.initializers.loop
+    .arguments[0] // FunctionExpression; TODO: reference
+    .body // BlockStatement
+    .body;
 
-fs.writeFileSync('game.json', JSON.stringify(game));
-
-fs.writeFileSync('game.ino', game.ino||'');
-
+  // Walk the setup-body contents
+  loopbody.forEach(exp => {
+    let ln = translate(exp);
+    game.loop.code.push(ln);
+    console.log('>>> '+ln);
+  });
+}
 
 
 function toSnakeCase(s) {
@@ -203,7 +234,9 @@ function isCalling(exp, id) {
   return false;
 }
 
-function getString(exp, target) {
+function getString(exp) {
+  if (typeof exp === 'string') return exp;
+
   if (!exp || !exp.type ) return '?';
 
   let self = getString;
@@ -219,7 +252,13 @@ function getString(exp, target) {
       return (exp.quasis.map(q => q.value.raw).join())
 
     case 'MemberExpression':
-      return (self(exp.object) + '.' + self(exp.property));
+      // Simple property access
+      if (exp.property.type === 'Identifier') {
+        return (self(exp.object) + '.' + self(exp.property));
+      }
+
+      // Property expression
+      return (self(exp.object) + '[ ' + self(exp.property)) +' ]';
 
     case 'NewExpression':
       return 'new '+self(exp.callee);
@@ -236,10 +275,12 @@ function getString(exp, target) {
   }
 }
 
-function translate(target, exp) {
+function translate(exp) {
+  if (typeof exp === 'string') return exp;
+
   if (!exp || !exp.type ) return '?';
 
-  let self = translate.bind(null, target);
+  let self = translate;
 
   switch (exp.type) {
 
@@ -249,7 +290,7 @@ function translate(target, exp) {
 
     // Look up identifiers
     case 'Identifier': // TODO: we'll need to handle scope (path)
-      return translateLookup(target, exp);
+      return lookup(exp);
 
     // Member expressions are usually translated to built-in methods
     case 'MemberExpression':
@@ -259,32 +300,73 @@ function translate(target, exp) {
       if (obj === game.alias
        || obj.match(/^(g|s)fx/) // Game asset properties (gfx & sfx)
      ) {
-        return translateLib(target, exp);
+        return translateLib(exp);
 
       // Some other library
       } else {
-        return '? ' + (self(exp.object) + '.' + self(exp.property));
+        // Simple property access
+        if (exp.property.type === 'Identifier') {
+          return '? ' + (self(exp.object) + '.' + self(exp.property));
+        }
+
+        // Property expression
+        return '? ' + (self(exp.object) + '[ ' + self(exp.property)) +' ]';
       }
 
     // Just unwrap expression body
     case 'ExpressionStatement':
       return self(exp.expression) +';';
 
+    // And wrap a block statement
+    case 'BlockStatement':
+      return '{\n'+exp.body.map(e => '  '+self(e)).join('\n') +'\n}';
+
+    // If statements are all the same
+    case 'IfStatement':
+      return 'if ('+self(exp.test)+') '
+        +( exp.consequent
+           ? self(exp.consequent) + ( exp.alternate ? self(exp.alternate) : '')
+           : ''
+        );
+
+    // Function calls
+    case 'CallExpression':
+      return translateLib(exp.callee, exp);
+
     // Assignment and binary expressions work pretty much unchanged across JS/C
+    case 'LogicalExpression': // TODO: parens?
     case 'BinaryExpression':
     case 'AssignmentExpression':
       return self(exp.left) +' '+exp.operator+' '+ self(exp.right);
+
+    // Unary expression (pre + postfix) work mostly the same
+    // TODO: boolean tricks? (!something >>> 1-something)
+    case 'UnaryExpression':
+      return (exp.prefix
+        ? exp.operator + self(exp.argument)
+        : self(exp.argument) + exp.operator
+      );
 
     default:
       return '? '+exp.type;
   }
 }
 
-function translateLib(target, exp) {
-  let obj = getString(exp.object),
-      prop = getString(exp.property);
+function translateArgs(args) {
+  return '('
+    +(args.length
+      ? ' '+args.map(arg => translate(arg)).join(', ')+' '
+      : ''
+    )
+  +')';
 
-  console.log('{%s.%s}', obj, prop);
+}
+
+function translateLib(exp, callexp) {
+  let obj = getString(exp.object),
+      prop = exp.property.type == 'Identifier' ? getString(exp.property) : exp.property;
+
+  if (typeof prop == 'string') console.log('{%s.%s}', obj, prop); else console.log('{%s[%s]}', obj, getString(prop));
 
   // Standard library (MicroCanvas) method/property
   if (obj === game.alias) {
@@ -295,6 +377,66 @@ function translateLib(target, exp) {
 
       // Global game state
       case 'state': return '_microcanvas_state';
+    }
+
+    // Function/library method calls
+    if (callexp) {
+      // Call to 1:1 library mapping library functions
+      if (~[
+        'everyXFrames',
+        'clear',
+      ].indexOf(prop)) {
+        return game.target+'.'+prop + translateArgs(callexp.arguments);
+      }
+
+      // strings in "buttonPressed" are turned into constants
+      if (prop === 'buttonPressed') {
+        // First argument is the queried button
+        let btn = getString(callexp.arguments[0]);
+
+        if (btn in BUTTONS[targetSystem]) {
+          return game.target+'.pressed' + translateArgs([ BUTTONS[targetSystem][btn] ]);
+        } else {
+          return new Error('Unknown button reference: '+btn);
+        }
+      }
+
+      // drawImage has a different library name and uses different args
+      // game.drawImage(gfx,x,y);    >>>    arduboy.drawBitmap(x,y, gfx, GFX_WIDTH,GFX_HEIGHT, WHITE);
+      // game.clearImage(gfx,x,y);   >>>    arduboy.drawBitmap(x,y, gfx, GFX_WIDTH,GFX_HEIGHT, BLACK);
+      if (prop === 'drawImage' || prop === 'clearImage' || prop === 'eraseImage') {
+        let sA = callexp.arguments;
+        let gfx = getString(sA[0].object);
+        let clear = (prop === 'clearImage' || prop === 'eraseImage');
+        let targetArgs = [
+          sA[1], sA[2], sA[0],
+          { type: 'MemberExpression', object: { type: 'Identifier', name: gfx }, property: { type: 'Identifier', name:'width' }},
+          { type: 'MemberExpression', object: { type: 'Identifier', name: gfx }, property: { type: 'Identifier', name: 'height' }},
+          clear ? 'BLACK' : 'WHITE'
+        ];
+
+        return game.target+'.drawBitmap' + translateArgs(targetArgs);
+        // todo subframe slice version
+      }
+
+      // drawText
+      // sprintf(_microcanvas_textbuffer, "DIST: %d", d);
+      // arduboy.setTextSize(3);
+      // arduboy.setCursor(0,0);
+      // arduboy.print("Sprite\nDemo");
+      // game.drawText("Sprite\nDemo", 0,0, 3);
+      if (prop === 'drawText') {
+        let sA = callexp.arguments;
+        // TODO: proper string/concat/cast/etc expression handling
+        let text = getString(sA[0]);
+
+        return (
+          ( sA[3] ? game.target+'.setTextSize'+translateArgs([ sA[3] ])+';\n' : '')+
+          game.target+'.setCursor'+translateArgs([ sA[1], sA[2] ])+';\n'+
+          game.target+'.print'+translateArgs([ sA[0] ])
+        );// todo subframe slice version
+      }
+
     }
 
   // Graphics asset property
@@ -308,23 +450,43 @@ function translateLib(target, exp) {
           return game.constants[id].cid;
         }
     }
+
+    // Computed property (e.g. frame access)
+    if (typeof prop == 'object') {
+      // TODO: proper type checking
+      return lookup(obj) + ' + ' + lookup(obj+'Framesize')+'*('+translate(prop)+')';
+    }
   }
 
   return '<'+getString(exp)+'>';
 }
 
-function translateLookup(target, exp) {
+function lookup(exp) {
   let id = getString(exp);
-  console.log('{%s}', id)
+
+  console.log('{%s}', id);
 
   // It's a global constant
-  if (game.constants[id]) {
+  if (id in game.constants) {
     return game.constants[id].cid;
   }
 
   // It's a global variable
-  if (game.globals[id]) {
+  if (id in game.globals) {
     return game.globals[id].cid;
+  }
+
+  // It's an asset
+  if (id in game.gfx) {
+    return game.gfx[id].cid;
+  }
+  if (id in game.sfx) {
+    return game.sfx[id].cid;
+  }
+
+  // It's a built-in library global or constant
+  if (id.match(/^(TRUE|FALSE|WIDTH|HEIGHT|WHITE|BLACK|INVERT)$/)) {
+    return id;
   }
 
   return '%'+id+'%';
@@ -376,6 +538,7 @@ function loadGraphics(id, args) {
   createConstant(id+'Width', game.gfx[id].meta.w);
   createConstant(id+'Height', game.gfx[id].meta.h);
   createConstant(id+'Frames', game.gfx[id].meta.frames);
+  createConstant(id+'Framesize', Math.ceil(game.gfx[id].meta.h/8)*game.gfx[id].meta.w);
 };
 
 function loadTune(id, args) {
@@ -417,8 +580,8 @@ function guessType(id, value, hint) {
   return 'int';
 }
 
-function exportGame(target) {
-  switch (target) {
+function exportGame() {
+  switch (game.target) {
     case 'arduboy':
       let b = '';
 
@@ -476,7 +639,7 @@ function exportGame(target) {
       b+=arduboySetup(game.setup.code.join('\n'))+'\n';
 
       // Loop
-      b+=arduboyLoop('')+'\n';
+      b+=arduboyLoop(game.loop.code.join('\n'))+'\n';
 
       // Functions
 
@@ -496,6 +659,9 @@ Arduboy arduboy;
 
 // frame counter, 2-byte unsigned int, max 65536
 unsigned int _microcanvas_frame_counter = 0;
+
+// sprintf() textbuffer for drawText
+char _microcanvas_textbuffer[32];
 
 // global state machine
 unsigned int _microcanvas_state;
